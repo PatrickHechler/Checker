a743d086043d6090b9f2898b42183ed4b6cfc9ca
Patrick <Patrick@Coruscant.Speedport_W_724V_01011603_07_002>
2021-06-01 22:56:45 +0200

    javadoc

--- src/main/java/de/hechler/patrick/zeugs/check/IntInt.java
+++ src/main/java/de/hechler/patrick/zeugs/check/IntInt.java
@@ -1,9 +1,19 @@
 package de.hechler.patrick.zeugs.check;
 
-
+/**
+ * this class is easily a wrapper about two primitiv {@code int} values
+ * 
+ * @author Patrick
+ */
 class IntInt {
 	
+	/**
+	 * the first <code>int</code> value
+	 */
 	int a;
+	/**
+	 * the second <code>int</code> value
+	 */
 	int b;
 	
 }
--- src/main/java/de/hechler/patrick/zeugs/check/Result.java
+++ src/main/java/de/hechler/patrick/zeugs/check/Result.java
@@ -1,18 +1,45 @@
 package de.hechler.patrick.zeugs.check;
 
+import java.lang.reflect.Method;
 import java.util.NoSuchElementException;
 
+/**
+ * saves the Result of a {@link Method#invoke(Object, Object...) Method call}. This is either an {@link Object} <code>return</code> result or an {@link Throwable} thrown result.
+ * 
+ * @author Patrick
+ *
+ */
 public final class Result {
 	
+	/**
+	 * this saves the return value (or <code>null</code> if it was a <code>void</code> method)<br>
+	 * if the method did not end normally (it throwed an {@link Throwable}) this value will be <code>null</code>
+	 */
 	private final Object    result;
+	/**
+	 * here will be the {@link Throwable} which was thrown by the called {@link Method}<br>
+	 * if the method ended normally this value will be <code>null</code>
+	 */
 	private final Throwable err;
 	
 	
+	/**
+	 * creates a {@link Result} with the {@link Object} as return value of a {@link Method}
+	 * 
+	 * @param e
+	 *            the return value of the executed {@link Method}
+	 */
 	public Result(Object e) {
 		result = e;
 		err = null;
 	}
 	
+	/**
+	 * creates a {@link Result} with the {@link Throwable} saved as error of a called {@link Method}
+	 * 
+	 * @param t
+	 *            the {@link Throwable} thrown while the {@link Method} was called
+	 */
 	public Result(Throwable t) {
 		result = null;
 		err = t;
@@ -20,19 +47,47 @@ public final class Result {
 	
 	
 	
+	/**
+	 * if this is a 'good' {@link Result} this {@link Method} will return <code>true</code>.<br>
+	 * This is a 'good' {@link Result} if no {@link Throwable} was thrown during or in other words if <code>{@link #err} == null</code> is <code>true</code>
+	 * 
+	 * @return <code>true</code> if this {@link Result} is a 'good' {@link Result}, <code>false</code> if not
+	 */
 	public boolean goodResult() {
 		return err == null;
 	}
 	
+	/**
+	 * if this is a 'bad' {@link Result} this {@link Method} will return <code>true</code>.<br>
+	 * This is a 'bad' {@link Result} if no {@link Throwable} was thrown during or in other words if <code>{@link #err} != null</code> is <code>true</code>
+	 * 
+	 * @return <code>true</code> if this {@link Result} is a 'bad' {@link Result}, <code>false</code> if not
+	 */
 	public boolean badResult() {
 		return err != null;
 	}
 	
+	/**
+	 * returns the saved return value of the {@link Method}.<br>
+	 * if this is no {@link #goodResult() good Result} it will throw a {@link NoSuchElementException}
+	 * 
+	 * @return the saved return value of the {@link Method}
+	 * @throws NoSuchElementException
+	 *             if this is a {@link #badResult() bad Result}
+	 */
 	public Object getResult() throws NoSuchElementException {
 		if (err != null) throw new NoSuchElementException("this is no good result!");
 		else return result;
 	}
 	
+	/**
+	 * returns the saved {@link #err} of the {@link Method}.<br>
+	 * if this is no {@link #badResult() bad Result} it will throw a {@link NoSuchElementException}
+	 * 
+	 * @return the saved {@link Throwable} of the {@link Method}
+	 * @throws NoSuchElementException
+	 *             if this is a {@link #goodResult() good Result}
+	 */
 	public Throwable getErr() throws NoSuchElementException {
 		if (err == null) throw new NoSuchElementException("this is no bad result!");
 		else return err;
@@ -45,7 +100,7 @@ public final class Result {
 		} else if (result != null) {
 			return "result[" + result + ']';
 		} else {
-			return "resultd null or void";
+			return "resuled null or void";
 		}
 	}
 	
